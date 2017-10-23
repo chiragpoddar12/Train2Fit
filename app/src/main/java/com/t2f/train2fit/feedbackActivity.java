b8b27235c81738631ba0f74c5bce1eeb0fb7d384
chiragpoddar12 <chiragpoddar12@gmail.com>
2017-10-23 18:23:49 +0100

    Added Feedback (Simple Implementation)

--- app/src/main/java/com/t2f/train2fit/feedbackActivity.java
+++ app/src/main/java/com/t2f/train2fit/feedbackActivity.java
@@ -1,6 +1,5 @@
 package com.t2f.train2fit;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,18 +13,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
-
-public class MainActivity extends AppCompatActivity
+public class feedbackActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_feedback);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -39,7 +35,7 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
+        drawer.setDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
@@ -86,8 +82,6 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
-            Intent bookingIntent=new Intent(MainActivity.this, BookingActivity.class);
-            startActivity(bookingIntent);
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
@@ -104,37 +98,4 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-
-    /** Called when the user taps the Login Button */
-    public void onLoginClick(View view) {
-        Intent intent = new Intent(this, LoginActivity.class);
-        String message = "From main activity to Login Activity";
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
-
-    /** Called when the user taps the Book Appointment Button */
-    public void onBookAppointmentClick(View view) {
-        Intent intent = new Intent(this, BookAppointmentActivity.class);
-        String message = "From main activity to Book Appointment Activity";
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
-
-    /** Called when the user taps on the Profile button*/
-    public void onProfileClick(View view) {
-        Intent intent = new Intent(this, ProfileActivity.class);
-        String message = "From main activity to Profile Activity";
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
-
-    /** Called when the Feedback button is clicked*/
-    public void onFeedbackClick(View view) {
-        Intent intent = new Intent(this, feedbackActivity.class);
-        String message = "From main activity to Feedback Activity";
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
-
 }
