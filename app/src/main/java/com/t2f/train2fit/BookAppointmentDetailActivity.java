b42e83f32dcbf4f710debe0ffa4a6348bdfa313c
Pavan Kulhalli <Pavan.Kulhalli@gmail.com>
2017-10-31 20:46:15 +0000

    Updated code for Appointment booking screen

--- app/src/main/java/com/t2f/train2fit/BookAppointmentDetailActivity.java
+++ app/src/main/java/com/t2f/train2fit/BookAppointmentDetailActivity.java
@@ -16,18 +16,33 @@
 
 package com.t2f.train2fit;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class BookAppointmentDetailActivity extends AppCompatActivity {
+
+    static EditText DateEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,11 +100,84 @@ public class BookAppointmentDetailActivity extends AppCompatActivity {
         // The detail Activity called via intent.  Inspect the intent for appointment data.
             Intent intent = getActivity().getIntent();
             if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
-                String appointmentStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+                String appointmentStr = "You Selected " + intent.getStringExtra(Intent.EXTRA_TEXT);
                 ((TextView) rootView.findViewById(R.id.detail_text)).setText(appointmentStr);
                 }
 
+            DateEdit = (EditText) rootView.findViewById(R.id.appointment_date);
+            DateEdit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    DialogFragment timeFragment = new TimePickerFragment();
+                    timeFragment.show(getActivity().getSupportFragmentManager(), "timePicker");
+                    DialogFragment dateFragment = new DatePickerFragment();
+                    dateFragment.show(getActivity().getSupportFragmentManager(), "datePicker");
+                }
+            });
+
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            DateEdit.setText(day + "/" + (month + 1) + "/" + year + " -" + hour + ":" + minute);
+
+            Button book_appointment_button = (Button) rootView.findViewById(R.id.book_appointment_button);
+            book_appointment_button.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v)  {
+                    Toast.makeText(getActivity().getBaseContext(), "Your Appointment booking is successful" , Toast.LENGTH_LONG ).show();
+//                    getActivity().finish();
+                    Intent intent = new Intent(getActivity(), MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }
+            });
+
             return rootView;
+        }
+    }
+
+    public static class DatePickerFragment extends DialogFragment implements
+            DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+            DateEdit.setText(day + "/" + (month + 1) + "/" + year);
+        }
+    }
+
+    public static class TimePickerFragment extends DialogFragment implements
+            TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            // Do something with the time chosen by the user
+            DateEdit.setText(DateEdit.getText() + " -" + hourOfDay + ":" + minute);
         }
     }
 }
---
+++ app/src/main/java/com/t2f/train2fit/TimePickerFragment.java
@@ -0,0 +1,36 @@
+package com.t2f.train2fit;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.text.format.DateFormat;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+public class TimePickerFragment extends DialogFragment
+        implements TimePickerDialog.OnTimeSetListener {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current time as the default values for the picker
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        // Create a new instance of TimePickerDialog and return it
+        return new TimePickerDialog(getActivity(), this, hour, minute,
+                DateFormat.is24HourFormat(getActivity()));
+    }
+
+
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        // Do something with the date chosen by the user
+        TextView appointment_date_textView= (TextView) getActivity().findViewById(R.id.appointment_date);
+        appointment_date_textView.setText(appointment_date_textView.getText()+" "+hourOfDay+":"+minute);
+    }
+}
