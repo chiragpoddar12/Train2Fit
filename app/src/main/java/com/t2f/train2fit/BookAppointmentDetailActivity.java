ddea33499c0de0c64ee1437332edbe1729c3f399
chiragpoddar12 <chiragpoddar12@gmail.com>
2017-11-29 14:17:11 +0000

    Assign trainer by location

--- app/src/main/java/com/t2f/train2fit/BookAppointmentDetailActivity.java
+++ app/src/main/java/com/t2f/train2fit/BookAppointmentDetailActivity.java
@@ -16,12 +16,19 @@
 
 package com.t2f.train2fit;
 
+import android.Manifest;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
@@ -48,7 +55,6 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.text.ParseException;
@@ -72,11 +78,33 @@ public class BookAppointmentDetailActivity extends AppCompatActivity {
     private static DatabaseReference mDatabase;
     private static FirebaseAuth auth;
     private FirebaseAuth.AuthStateListener authListener;
+    public LocationManager lm;
+    public LocationListener locationListener;
+    public static Location location;
     static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy - HH:mm", Locale.US);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.book_appointment_detail);
+
+        lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+//            return;
+        } else {
+
+            configureLocation();
+        }
+//        lm.requestLocationUpdates("gps", 5000, 0, locationListener);
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
@@ -85,6 +113,28 @@ public class BookAppointmentDetailActivity extends AppCompatActivity {
         }
     }
 
+    public void configureLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+//            return;
+        }
+        location = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1 : configureLocation();
+            break;
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -120,7 +170,6 @@ public class BookAppointmentDetailActivity extends AppCompatActivity {
         private EditText appointment_date;
         private EditText appointment_notes;
         DatabaseReference mDatabase;
-        private String bookingId;
         public DetailFragment() {
         }
 
@@ -129,20 +178,14 @@ public class BookAppointmentDetailActivity extends AppCompatActivity {
                                  Bundle savedInstanceState) {
 
             final View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
-            bookingId = "";
+
         // The detail Activity called via intent.  Inspect the intent for appointment data.
             Intent intent = getActivity().getIntent();
             if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
                 String appointmentStr = "You Selected " + intent.getStringExtra(Intent.EXTRA_TEXT);
                 ((TextView) rootView.findViewById(R.id.detail_text)).setText(appointmentStr);
-                } else  {
-                String appointmentStr = "You Selected " + intent.getStringExtra("trainerType");
+                }
 
-                ((TextView) rootView.findViewById(R.id.detail_text)).setText(appointmentStr);
-                ((TextView) rootView.findViewById(R.id.appointment_date)).setText(intent.getStringExtra("date"));
-                ((TextView) rootView.findViewById(R.id.appointment_notes)).setText(intent.getStringExtra("notes"));
-                bookingId = intent.getStringExtra("bookingId");
-            }
             DateEdit = (EditText) rootView.findViewById(R.id.appointment_date);
             DateEdit.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -168,9 +211,8 @@ public class BookAppointmentDetailActivity extends AppCompatActivity {
             int hour = c.get(Calendar.HOUR_OF_DAY);
             int minute = c.get(Calendar.MINUTE);
 
-            if (bookingId.toString() == "") {
-                DateEdit.setText(day + "/" + (month + 1) + "/" + year + " -" + hour + ":" + minute);
-            }
+            DateEdit.setText(day + "/" + (month + 1) + "/" + year + " -" + hour + ":" + minute);
+
 
             mDatabase = FirebaseDatabase.getInstance().getReference().child("Bookings");
             final Button book_appointment_button = (Button) rootView.findViewById(R.id.book_appointment_button);
@@ -185,33 +227,51 @@ public class BookAppointmentDetailActivity extends AppCompatActivity {
                     String userId = user.getUid();
 //                    String userId = user.getUid(); //retrieve from session
                     final String trainerType = ((TextView) rootView.findViewById(R.id.detail_text)).getText().toString().split("You Selected")[1];
-                    final String date_time = ((EditText) rootView.findViewById(R.id.appointment_date)).getText().toString();
-                    final String notes = ((EditText) rootView.findViewById(R.id.appointment_notes)).getText().toString();
+                    String date_time = ((EditText) rootView.findViewById(R.id.appointment_date)).getText().toString();
+                    String notes = ((EditText) rootView.findViewById(R.id.appointment_notes)).getText().toString();
                     final Map<String, String> booking = new HashMap();
+                    final float distance = 0;
                     booking.put("trainerType", trainerType);
                     booking.put("dateTime", date_time);
                     booking.put("notes", notes);
                     booking.put("user", userId);
+
+
                     mDatabase.getParent().child("Trainers").addListenerForSingleValueEvent(new ValueEventListener() {
                         @Override
                         public void onDataChange(DataSnapshot dataSnapshot) {
-                            Map<String, Object> trainers = (Map<String, Object>) dataSnapshot.getValue();
-                            for (Map.Entry<String, Object> trainer : trainers.entrySet()) {
+                            Map<String,Object> trainers = (Map<String,Object>) dataSnapshot.getValue();
+                            double minDist = 999999999;
+                            String trainerId = "";
+                            for (Map.Entry<String, Object> trainer : trainers.entrySet()){
                                 Map trainerData = (Map) trainer.getValue();
                                 System.out.println(trainerData.get("trainerType").toString());
                                 System.out.println(trainerType);
-                                if (trainerType.trim().equals(trainerData.get("trainerType").toString().trim())) {
-                                    String trainerId = trainerData.get("trainerId").toString();
-                                    booking.put("trainerId", trainerId);
-                                    break;
+                                if(trainerType.trim().equals(trainerData.get("trainerType").toString().trim())){
+                                    if(location != null){
+                                        Location trainerLocation = new Location("trainer");
+                                        float trainerLat = Float.parseFloat(trainerData.get("lat").toString());
+                                        float trainerLng = Float.parseFloat(trainerData.get("lng").toString());
+                                        trainerLocation.setLatitude(trainerLat);
+                                        trainerLocation.setLongitude(trainerLng);
+
+                                        double distance = location.distanceTo(trainerLocation);
+                                        if(distance < minDist) {
+                                            minDist = distance;
+                                            trainerId = trainerData.get("trainerId").toString();
+                                        }
+                                    }else{
+                                        Toast.makeText(getActivity(),"Switch on your location tracker" , Toast.LENGTH_LONG ).show();
+                                        return;
+                                    }
                                 }
                             }
-                            if (bookingId.toString() == "") {
+                            booking.put("trainerId", trainerId);
                             mDatabase.push().setValue(booking).addOnSuccessListener(new OnSuccessListener<Void>() {
                                 @Override
                                 public void onSuccess(@NonNull Void T) {
-                                    Toast.makeText(getActivity().getBaseContext(), "Your Appointment booking is successful", Toast.LENGTH_LONG).show();
-                                    //                          getActivity().finish();
+                                    Toast.makeText(getActivity().getBaseContext(), "Your Appointment booking is successful" , Toast.LENGTH_LONG ).show();
+        //                          getActivity().finish();
                                     Intent intent = new Intent(getActivity(), ProfileActivity.class);
                                     intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                                     startActivity(intent);
@@ -219,23 +279,9 @@ public class BookAppointmentDetailActivity extends AppCompatActivity {
                             }).addOnFailureListener(new OnFailureListener() {
                                 @Override
                                 public void onFailure(@NonNull Exception e) {
-                                    Toast.makeText(getActivity().getBaseContext(), "Error : Your Appointment booking is Unsuccessful", Toast.LENGTH_LONG).show();
+                                    Toast.makeText(getActivity().getBaseContext(), "Error : Your Appointment booking is Unsuccessful" , Toast.LENGTH_LONG ).show();
                                 }
                             });
-                        } else {
-                                try {
-                                    mDatabase.child(bookingId).child("dateTime").setValue(date_time);
-                                    mDatabase.child(bookingId).child("notes").setValue(notes);
-
-                                    Toast.makeText(getActivity().getBaseContext(), "Your Appointment reschedule was successful", Toast.LENGTH_LONG).show();
-                                    //                          getActivity().finish();
-                                    Intent intent = new Intent(getActivity(), ProfileActivity.class);
-                                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                                    startActivity(intent);
-                                } catch (Exception e) {
-                                    e.printStackTrace();
-                                }
-                            }
                         }
 
                         @Override
